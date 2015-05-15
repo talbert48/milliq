0f92d69ebac8f32def1522b9ad895f9611bcd75a
talbert48 <talber.48@osu.edu>
2015-05-15 12:13:08 -0400

    Added plotting options
    Added in the ability to make a number of different plots. Pulse area
    histogram, pulse max/min histogram. Scatter plots looking at pulse area
    vs pulse maximum.

--- Signal Processing/PMT_Analyze.py
+++ Signal Processing/PMT_Analyze.py
@@ -4,15 +4,33 @@ import matplotlib.pyplot as plt
 NTraces = 1000
 NPoints = 1024
 
-def AnalyzeFile(filename):
+def AnalyzeFile(filename,type):
     print('Analyzing ' + filename)
     data = ReadData(filename)
     sums = np.zeros(NTraces)
+    maxs = np.zeros(NTraces)
+    Tail = np.zeros(NTraces)
+    outData = np.zeros((3,NTraces))
     for i in range(NTraces):
-        data[i,:] = data[i,:] - Offset(data[i,0:75])             
-        sums[i] = data[i,:].sum()  
-        #plotEvent(data[i,:])
-    return sums
+        #data[i,:] = data[i,:] - Offset(data[i,0:75])
+        data[i,:] = data[i,:] - np.amin(data[i,200:])
+        data[i,:] = data[i,:] - Offset(data[i,200:])
+        if np.amax(data[i,:]) < 7: # zero out garbage events
+            data[i,np.arange(0,NPoints)]=0
+            continue
+        if type == 0:
+            sums[i] = data[i,:].sum()
+        else:
+            sums[i] = data[i,90:250].sum()
+        # if sums[i] < 0:
+        plotEvent(data[i,:])            
+        maxs[i] = np.amax(data[i,:])
+        Tail[i] = data[i,110:250].sum();
+        outData[0,i] = sums[i]
+        outData[1,i] = maxs[i]
+        outData[2,i] = Tail[i]/sums[i]
+        #outData[2,i] = np.amin(data[i,:])
+    return outData
         
     
 def ReadData(filename):
@@ -28,13 +46,17 @@ def ReadData(filename):
     return data
     
 def Offset(data):
-    hist, bin = np.histogram(data, bins = np.arange(-10,11))
+    hist, bin = np.histogram(data, bins = np.arange(-10,10))
     bin = np.delete(bin,-1)
     offset = (hist * bin).sum() / hist.sum()
     return offset
 
 def plotEvent(data):
-        plt.plot(data)
+        x = np.linspace(0.0,12.8, num = NPoints)
+        plt.plot(x,data)
+        plt.xlabel('$\mu$s')
+        plt.ylabel('mV')
+        plt.title('PMT Pulse')
         plt.show()
         plt.waitforbuttonpress()
         plt.clf()
--- Signal Processing/PMT_Main.py
+++ Signal Processing/PMT_Main.py
@@ -3,23 +3,54 @@ import PMT_Analyze
 import numpy as np
 import matplotlib.pyplot as plt
 
-#set items for binning and plotting
-x = np.arange(0,5000,10)
-bincenters = 0.5*(x[1:]+x[:-1])
+#set items for binning and plotting integration
+x = np.arange(0,30000,5)
+bincenters = 0.5*(x[1:]+x[:-1]) * 12.8 / 1024 # convert from ADC to micro seconds
 
-dir = ['D:\\PMT_Analysis\\Data\\May12\\1200V','D:\\PMT_Analysis\\Data\\May12\\1200VDark','D:\\PMT_Analysis\\Data\\May12\\1200VAgain'] # Set directory for data analysis
+#set items for binning and plotting maximums
+x2 = np.arange(0,1100,1)
+bincentersMaxs = 0.5*(x2[1:]+x2[:-1])
+
+colors = ['blue', 'green', 'magenta', 'cyan']
+
+dir = ['D:\\PMT_Analysis\\Data\\May14\\PulserHigh','D:\\PMT_Analysis\\Data\\May13\\LEDonTriggerOn'] # Set directory for data analysis
 for i in range(len(dir)):
-    sumTemp = np.zeros(1)
+    dataTemp = np.zeros((3,1))
     for file in os.listdir(dir[i]): # Look at every file in directory
         if file.endswith(".dat"): # Only care about certain extension types
             filename = dir[i] + '\\' + file
-            sumTemp = np.hstack((sumTemp,PMT_Analyze.AnalyzeFile(filename))) #analyze file and add it to the overall data array
-    sumTemp = np.delete(sumTemp,0) # remove garbage element
-    y, x = np.histogram(sumTemp, bins = x)
+            dataTemp = np.hstack((dataTemp,PMT_Analyze.AnalyzeFile(filename , 0))) #analyze file and add it to the overall data array
+            
+    dataTemp = dataTemp[:,1:] # remove garbage element
+    goodData = np.array(dataTemp[0,:]) != 0
+    plt.figure('Sums')
+    y, x = np.histogram(dataTemp[0,goodData], bins = x)
     plt.plot(bincenters, y)
+    plt.figure('Maxs')
+    y, x2 = np.histogram(dataTemp[1,goodData], bins = x2)
+    plt.plot(bincentersMaxs, y)
+    plt.figure('Scatter')
+    plt.scatter(dataTemp[0,:],dataTemp[1,:],c=colors[i])
 
+legends = ['Background','LED On']
+#plot figures
+plt.figure('Sums')
 plt.xlabel('mV $\mu$s')
-plt.ylabel('Counts per 5 minute')
-plt.title('Integration Histogram')
-plt.legend(['1200 V w/ Scintillator, w/ Lights on','1200 V w/ Scintillator, w/ Lights off','1200 V w/ Scintillator, w/ Lights on run 2'])
+plt.ylabel('Counts per 5 minutes')
+plt.title('Pulse Area Histogram')
+plt.legend(legends)
+plt.show()
+
+plt.figure('Maxs')
+plt.xlabel('mV')
+plt.ylabel('Counts per 5 minutes')
+plt.title('Pulse Maximum Histogram')
+plt.legend(legends)
+plt.show()
+
+plt.figure('Scatter')
+plt.legend(legends)
+plt.xlabel('Pulse Area')
+#plt.ylabel('Pulse Tail Area/ Total Area')
+plt.ylabel('Pulse Maximum (mV)')
 plt.show()

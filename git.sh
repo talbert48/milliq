fcc802f898224ea0e6b2b3d568c66155917b8ce5
qftsm <gabriel.magill1@gmail.com>
2015-12-18 22:21:39 -0500

    Analyses everything, root script setup, outputs to mathematica

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add .
+git add --all .
 git commit -m 'Analyses everything, root script setup, outputs to mathematica'
 # git remote add origin https://github.com/talbert48/milliq.git
 git remote -v

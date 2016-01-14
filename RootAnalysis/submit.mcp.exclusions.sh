5a61194f9bd75d53e59f270811c950d18c6911dc
Gabriel Magill <gmagill@perimeterinstitute.ca>
2016-01-14 17:14:38 -0500

    added light guide shell

--- RootAnalysis/submit.mcp.exclusions.sh
+++ RootAnalysis/submit.mcp.exclusions.sh
@@ -11,22 +11,26 @@ JOB=$BUILD/Job."$rseed"
 charge=$1
 mass=$2
 nEv=$3
-RESULTS=/xfs1/gmagill/Repository_MilliCharged/Geant4/MCPRepo/EffStudy
-#masses=(100.0) #1.0 10.0 100.0)
-sourcecharge=0.001
-process=(mCP_UFO)
+proc=$4
+RESULTS=/xfs1/gmagill/Repository_MilliCharged/Geant4/MCPRepo/FullRun
+
+cnew="$(echo $charge | sed 's/0.//')"
+if [ "$cnew" -lt "0100" ]; then
+        sourcecharge=0.001
+elif [ "$cnew" -lt "1000" ]; then
+        sourcecharge=0.01
+elif [ "$cnew" -ge "1000" ]; then
+        sourcecharge=0.1
+fi
+
 
 mkdir $JOB
 
-#for mass in ${masses[*]}
-#do
 for proc in ${process[*]}
 do
 	echo Mass:$j GeV Charge:$i
 	outputname="$proc"."$mass"GeV."$charge"Q
 	sourcename="$proc"/"$mass"/"$sourcecharge"/hit_4_vecs.txt
-#	nEv=$(cat $DATA/"$sourcename".txt | wc -l)
-#	nEv=300
 	cp $ROOT/mcp.mac $JOB
 	cp $ROOT/histogram.C $JOB
 	cp -r $HOME/milliq/geant4 $JOB
@@ -43,11 +47,13 @@ do
 	make MilliQ
 	./MilliQ mcp.mac
 	root -b -q histogram.C
-#	mv mcpall.dat $RESULTS/mcpall."$outputname".dat 
+	mv mcpall.dat $RESULTS/mcpall."$outputname".dat 
 #	mv sedep.dat $RESULTS/sedep."$outputname".dat
+	mv pmt0time.dat $RESULTS/pmt0time."$outputname".dat
+        mv pmt1time.dat $RESULTS/pmt1time."$outputname".dat
+        mv pmt2time.dat $RESULTS/pmt2time."$outputname".dat
 	nPass=$(cat mcpall.dat | wc -l)
-	echo $mass $charge $nEv $nPass >> $RESULTS/EffStudy5by10refl0.98att210lgrefl1.dat
+	echo $mass $charge $nEv $nPass >> $RESULTS/Acceptances.dat
 	cd ../
 	rm -r $JOB
 done
-#done

e77212ce377f639d8dd0d4c2eb2f1b34ef9265f5
Gabriel Magill <gmagill@perimeterinstitute.ca>
2016-01-21 12:53:20 -0500

    update

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add --all .
-git commit -m 'added light guide shell'
+git commit -m 'update'
 #git remote add origin https://github.com/talbert48/milliq.git
 git remote -v
 git push origin master

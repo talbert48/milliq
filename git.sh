517af38463ad1bc4b0f0c4db08d83b904aeb19e2
Gabriel Magill <gmagill@perimeterinstitute.ca>
2016-03-15 16:43:02 -0400

    update

--- git.sh
+++ git.sh
@@ -3,6 +3,7 @@
 git add --all .
 git commit -m 'update'
 #git remote add origin https://github.com/talbert48/milliq.git
+git remote add origin https://github.com/gabrielmagill/milliq.git
 git remote -v
 git push origin master
 
